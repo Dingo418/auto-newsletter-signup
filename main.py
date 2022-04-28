d85100fe0216dfe64f99debe9ffdc0a249b25a41
Dingo418 <56755081+Dingo418@users.noreply.github.com>
2022-04-28 18:38:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ def main():
     #repeats for the whole list
     for i in range(0, len(d.data.keys())):
         data = d.data[i]
-        if data.get("type") == 1:
-            print("type 1")
+        if data.get("type") == "get":
+            print("get")
             push = requests.get(url=data.get("url"), params=data.get("payload"))
             print(f"{g.email} was succesfully subscribed to {data.get('name')} Newsletter")
             print(push.text)
-        elif data.get("type") == 2:
-            print("type 2")
+        elif data.get("post") == 2:
+            print("post")
             push = requests.post(url=data.get("url"), data=data.get("payload"))
             print(push.text)
             print(f"{g.email} was succesfully subscribed to {data.get('name')} Newsletter")
-        elif data.get("type") == 3:
-            print("3")
+        elif data.get("post_json") == 3:
+            print("post_json")
             break
         else: 
-            print("error")
+            print("error, no type listed ")
 
 if __name__ == "__main__":
     main()
